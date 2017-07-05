e62bb924769d5eb9f0353d442ba739126ce0ff79
soloanakin51 <soloanakin51@gmail.com>
2017-07-05 17:23:04 -0300

    Primera subida.
    No hay nada concreto, más que una prueba de cabecera de página.

---
+++ header.php
@@ -0,0 +1,25 @@
+
+<?php
+
+?>
+<div class="header">
+    <div class="logo">
+        
+    </div>
+    <div class="conteiner">
+        <div class="title">
+            <h1>CHCX en 3D</h1>
+        </div>
+        <div class="searchnuser">
+            <div class="search">
+                <form>
+                    <input type="text" name="Buscar" class="buscararea" />
+                    <input class="botonbuscar" type="submit" name="buscarbot" value="">
+                </form>
+            </div>
+            <div class="user">
+                <p>Usuario</p>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="estilo.css" type="text/css" />
+        <link rel="shortcut icon" href="favicon.ico">
+        <title>CHCX en 3D</title>
+    </head>
+    <body>
+        <?php
+        include './header.php';
+        ?>
+    </body>
+</html>
